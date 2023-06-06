7db1fd88b5884c6aa95e9c728db44be21c829c44
Jimin Yoo <jmy0819_@naver.com>
2023-06-06 16:50:14 +0900

    숫자

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class SpriteChanger : MonoBehaviour {
+
+    Image currentImage;
+    public Sprite[] imageArray;
+    public GameManager gameManager;
+    [SerializeField] private int digit;
+    [SerializeField] private bool isFish;
+    int itemCount;
+
+    void Start() {
+        currentImage = GetComponent<Image>();
+        itemCount = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        string tag = (isFish) ? "Fish" : "Bomb";
+        itemCount = gameManager.itemInfo.ContainsKey(tag) ? gameManager.itemInfo[tag] : 0;
+
+        ChangeSprite(itemCount);
+    }
+
+    void ChangeSprite(int num) {
+        num /= (int) Math.Pow(10, digit);
+        num %= 10;
+
+        currentImage.sprite = imageArray[num];
     }
 }
